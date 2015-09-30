7d80020e71ecbd351651f7e5caeda2b4af9f7c64
Ron Elliott <ron@elliott.im>
2015-09-30 13:12:46 -0700

    Updated handler names.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 module.exports = function($$resolver) {
     $$resolver.add({
-        'mongo-collection-create': require('./handlers/mongo-collection-create'),
-        'mongo-collection-delete': require('./handlers/mongo-collection-delete'),
-        'mongo-collection-get': require('./handlers/mongo-collection-get'),
-        'mongo-collection-list': require('./handlers/mongo-collection-list'),
-        'mongo-collection-update': require('./handlers/mongo-collection-update')
+        '$mongo-collection-create': require('./handlers/mongo-collection-create'),
+        '$mongo-collection-delete': require('./handlers/mongo-collection-delete'),
+        '$mongo-collection-get': require('./handlers/mongo-collection-get'),
+        '$mongo-collection-list': require('./handlers/mongo-collection-list'),
+        '$mongo-collection-update': require('./handlers/mongo-collection-update')
     });
 };
